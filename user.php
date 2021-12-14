db388c7437274fa9bdb5e9ed0d50de337fa7db9a
ifanl-ibrahim <ifanl.ibrahim@laplateforme.io>
2021-12-14 21:14:18 +0100

    ajout construct

---
+++ user.php
@@ -0,0 +1,29 @@
+<?php 
+
+class user {
+    public $login;
+    public $email;
+    public $firstname;
+    public $lastname;
+
+// construct
+
+public function _constuct () {}
+
+//register
+
+public function _register ($login, $password, $email, $firstname, $lastname) {}
+
+
+
+
+
+
+
+
+
+
+
+}
+
+?>
